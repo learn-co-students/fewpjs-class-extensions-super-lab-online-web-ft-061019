f0c402b085e01bedb35ea36577e30cd75aae9d49
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-10-15 13:59:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your classes here
+// class Tree {
+//     constructor(species){
+//         this.species = species
+//     }
+//     static definition(){
+//         return `A ${treeDef.all} is a perennial plant with an elongated stem, or trunk, supporting branches and leaves.`
+//     }
+// }
+// class Deciduous extends Tree {
+//     constructor(species, name)
+// }
